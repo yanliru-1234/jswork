d7aeacffae2bd99cd122f0df174bf6627f1ad740
yanliru-1234 <2312862225qq.com>
2019-10-14 15:51:21 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,16 +1,34 @@
-//9X9的空表，请自行编写这段代码
-
-
+//9X9的空表
+str = '<table border="1">'
+for(let i=1; i<10;++i){
+    str += '<tr>'
+    for(var j=1; j<10;++j){
+        str += '<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str    
 //倒九九乘法空表
+
 str = '<table border="1">'
 for(let i=9;i>0;--i){
-    str += '<tr>'
     for(var j=i;j>0;--j){
         str += '<td>&nbsp;</td>'
     }
     str +='</tr>'
 }
-str+= '</table>'
+str += '</table>'    
 document.getElementById('table2').innerHTML = str
 
-//倒九九乘法表 ， 请自行编写这段代码
+//倒九九乘法空表 
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    for(var j=i;j>0;--j){
+        str += '<td>' + i + '*' + j + '=' + ( i * j) + '</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'    
+document.getElementById('table3').innerHTML = str
+
