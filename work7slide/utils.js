873163db9f5e82cfaab00501ac69cbc72580f880
apple <apple@jd505-49.local>
2019-10-21 15:50:46 +0800

    work7

---
+++ work7slide/utils.js
@@ -0,0 +1,12 @@
+//冒泡排序
+function bubbleSort(str) {
+    let arr = str.split(',')
+    let sortLog = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            sortLog.push([arr.concat(),[j, j + 1]])
+            sortLog.push([arr.concat(),[j, j + 1]])
+            sortLog.push([arr.concat(),[j, j + 1]])          
+        }
+    }
+}
