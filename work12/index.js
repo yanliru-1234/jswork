384508779cdc0e4e1529ed293f277d5568d0475e
yanliru-1234 <2312862225@qq.com>
2019-11-25 22:59:20 +0800

    work12

--- work12/index.js
+++ work12/index.js
@@ -1,10 +1,21 @@
 //获取当前年份
 let year = new Date().getFullYear()
-//调用calender函数，设置cal内容显示年历
-document.getElementById("cal").innerHTML = calender(year)
-function setCalendar(year){
-    //设置preyear
-    //设置curyear
-    //设置nextyear
-    //设置cal显示年历
+
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year) {
+    document.getElementById('cal').innerHTML = '<div class="box" id="cal"></div>'
+
+    var year = parseInt(year)
+    var pre = year-1
+    var next = year+1
+    console.log(pre)
+    console.log(next)
+    document.getElementById('curyear').setAttribute('data-year', year);
+    document.getElementById('preyear').setAttribute('data-year', pre);
+    document.getElementById('nextyear').setAttribute('data-year', next);
+    document.getElementById('curyear').innerText = year
+    document.getElementById('preyear').innerText = "<<"+pre
+    document.getElementById('nextyear').innerText = next+ ">>"
+    document.getElementById("cal").innerHTML = calendar(year)
 }
