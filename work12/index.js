ca5b11a9236f3ba9654d413f4ad4210718b5ec7a
apple <apple@jd505-49.local>
2019-11-25 15:49:18 +0800

    work12

---
+++ work12/index.js
@@ -0,0 +1,10 @@
+//获取当前年份
+let year = new Date().getFullYear()
+//调用calender函数，设置cal内容显示年历
+document.getElementById("cal").innerHTML = calender(year)
+function setCalendar(year){
+    //设置preyear
+    //设置curyear
+    //设置nextyear
+    //设置cal显示年历
+}
