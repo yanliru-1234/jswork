f67b80f52c1721f78d9043bda996abdbc0a176ef
yanliru-1234 <2312862225@qq.com>
2019-12-04 21:58:32 +0800

    work14

--- work14/index.js
+++ work14/index.js
@@ -1,26 +1,33 @@
-let endseconds = new Date().getTime() + 15 * 1000;
+//let endseconds =  - new Date().getMinutes();
+
 let d = h = m = s = 0;
+
 let id = setInterval(seckill, 1000);
 
-function seckill(){
+function seckill() {
+    let endseconds = new Date().getMinutes();
     let nowtime = new Date();
-    let remaining = parseInt((endseconds - nowtime.getTime()) /1000);
+    let remaining = parseInt((60 - endseconds) *60);
+    var xs =  nowtime.getSeconds();
+    var ms = parseInt(60 - xs);//这里获取倒计时的秒数 
+    console.log(ms);
+    //console.log(ms);为什么是Nan？？？？ 这里是控制台第二个数字
     if (remaining > 0) {
         d = parseInt(remaining / 86400);
         h = parseInt((remaining / 3600) % 24);
         m = parseInt((remaining / 60) % 60);
-        s = parseInt(remaining % 60);
-
-        d= d < 10 ? '0' + d : d;
-        h= h< 10 ? '0' + h : h;
-        m= m < 10 ? '0' + m : m;
-        s= s < 10 ? '0' + s : s;
-    } else{
-        clearInterval(id);
-        d=h=m=s='00';
+        s = parseInt(ms);
+        d = d < 10 ? '0' + d : d;
+        h = h < 10 ? '0' + h : h;
+        m = m < 10 ? '0' + m : m;
+        s = s < 10 ? '0' + s : s;
+    } else {
+        clearInterval(id);//秒杀过期就取消
+        d = h = m = s ='00';
     }
-    document.getElementById('d').innerHTML = d + '天';
-    document.getElementById('h').innerHTML = d + '时';
-    document.getElementById('m').innerHTML = d + '分';
-    document.getElementById('s').innerHTML = d + '秒';
+//将剩下的时间显示在指定的位置中
+document.getElementById('d').innerHTML = d + '天';
+document.getElementById('h').innerHTML = h + '时';
+document.getElementById('m').innerHTML = m + '分';
+document.getElementById('s').innerHTML = s + '秒';
 }
