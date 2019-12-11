30950e768c3317ea13627c15e5955975590c5f8d
yanliru-1234 <2312862225@qq.com>
2019-12-11 13:59:51 +0800

    work15

--- work15/ShopCart.js
+++ work15/ShopCart.js
@@ -1,119 +1,96 @@
-(function(window) {
-    let ShopCart = function(prefix, defCart) {
+(function (window) {
+  let ShopCart = function (prefix, defCart) {
       Find.prototype.prefix = prefix;
       let cart = new Cart(document.getElementsByClassName(prefix)[0]);
       for (let i in defCart) {
-        cart.add(defCart[i]);
+          cart.add(defCart[i]);
       }
       cart.updateTotal();
-    };
-    function Cart(obj) {
+  };
+  function Cart(obj) {
       this.items = [];
       let find = new Find(obj);
       this.all = find.className('all');
       this.unall = find.className('unall');
-      this.agcheck = find.className('agcheck');
       this.bottom = find.className('bottom');
       this.num = find.className('total-num');
       this.price = find.className('total-price');
       this.tmp = find.className('item');
       this.tmp.parentNode.removeChild(this.tmp);
       let cart = this;
-      this.all.onclick = function() {
-        cart.checkAll();
+      this.all.onclick = function () {
+          cart.checkAll();
       };
-      this.unall.onclick = function() {
-        cart.uncheckAll();
-      };
-      this.agcheck.onclick = function() {
-        cart.acheck();
+      this.unall.onclick = function () {
+          cart.uncheckAll();
       }
-    }
-    Cart.prototype = {
-  
-      add: function(data) {
-        let tmp = this.tmp.cloneNode(true);
-        let item = new Item(tmp, data);
-        let cart = this;
-     
-        item.check.onclick = function () {
-          cart.updateTotal();
-        };
-  
-        item.add.onclick = function() {
-          item.num.textContent = ++item.data.num;
-          item.updateSubtotal()
-          cart.updateTotal();
-        };
-        
-        item.reduce.onclick = function() {
-          if (item.data.num > 1) {
-            item.num.textContent = --item.data.num;
-            item.updateSubtotal();
-            cart.updateTotal();
-          } else {
-            alert('至少选择1件，如果不需要，请直接删除');
+  }
+  Cart.prototype = {
+      add: function (data) {
+          let tmp = this.tmp.cloneNode(true);
+          let item = new Item(tmp, data);
+          let cart = this;
+          item.check.onclick = function () {
+              cart.updateTotal();
+          };
+          item.add.onclick = function () {
+              item.num.textContent = ++item.data.num;
+              item.updateSubtotal();
+              cart.updateTotal();
+          };
+          item.reduce.onclick = function () {
+              if (item.data.num > 1) {
+                  item.num.textContent = --item.data.num;
+                  item.updateSubtotal();
+                  cart.updateTotal();
+              } else {
+                  alert("至少选择1件，如果不需要，请直接删除");
+              }
+          };
+          item.del.onclick = function () {
+              if (confirm("您确定要删除此商品吗？")) {
+                  tmp.parentNode.removeChild(tmp);
+                  cart.del(item);
+                  cart.updateTotal();
+              }
+          };
+          item.updateSubtotal();
+          this.items.push(item);
+          this.bottom.before(tmp);
+      },
+      del: function (item) {
+          for (let i in this.items) {
+              if (this.items[i] === item) {
+                  delete this.items[i];
+              }
           }
-        };
-        
-        item.del.onclick = function() {
-          if (confirm('您确定要删除此商品吗？')) {
-            tmp.parentNode.removeChild(tmp);
-            cart.del(item);
-            cart.updateTotal();
+      },
+      updateTotal: function () {
+          let num = 0, price = 0;
+          for (let i in this.items) {
+              let item = this.items[i];
+              if (item.check.checked) {
+                  num += item.data.num;
+                  price += item.data.num * item.data.price;
+              }
           }
-        };
-    
-        item.updateSubtotal();
-       
-        this.items.push(item);
-        this.bottom.before(tmp);
+          this.num.textContent = num;
+          this.price.textContent = price.toFixed(2);
       },
-      
-  
-      del: function(item) {
-        for (let i in this.items) {
-          if (this.items[i] === item) {
-            delete this.items[i];
+      checkAll: function () {
+          for (let i in this.items) {
+              this.items[i].check.checked = true;
           }
-        }
+          this.updateTotal();
       },
-      
-      updateTotal: function() {
-        let num = 0, price = 0;
-        for (let i in this.items) {
-          let item = this.items[i];
-          if (item.check.checked) {
-            num += item.data.num;
-            price += item.data.num * item.data.price;
+      uncheckAll: function () {
+          for (let i in this.items) {
+              this.items[i].check.checked = false;
           }
-        }
-        this.num.textContent = num;
-        this.price.textContent = price.toFixed(2);
-      },
-     
-      checkAll: function() {
-        for (let i in this.items) {
-          this.items[i].check.checked = true;
-        }
-        this.updateTotal();
-      },
-      uncheckAll: function() {
-        for (let i in this.items) {
-          this.items[i].check.checked = false;
-        }
-        this.updateTotal();
-      },
-      acheck: function() {
-        for (let i in this.items) {
-          this.items[i].check.checked = this.items[i].check.checked ? false : true;
-        }
-        this.updateTotal();
-      },
-      
-    };
-  
-    function Item(tmp, data) {
+          this.updateTotal();
+      }
+  };
+  function Item(tmp, data) {
       let find = new Find(tmp);
       this.check = find.className('check');
       this.name = find.className('name');
@@ -127,23 +104,20 @@
       this.name.textContent = data.name;
       this.price.textContent = data.price.toFixed(2);
       this.num.textContent = data.num;
-    }
-    Item.prototype = {
-    
-      updateSubtotal: function() {
-        this.subtotal.textContent = (this.data.num * this.data.price).toFixed(2);
+  }
+  Item.prototype = {
+      updateSubtotal: function (obj) {
+          this.subtotal.textContent = (this.data.num * this.data.price).toFixed(2);
       }
-    };
-    
-    function Find(obj) {
+  };
+  function Find(obj) {
       this.obj = obj;
-    }
-    Find.prototype = {
+  }
+  Find.prototype = {
       prefix: '',
-   
-      className: function(className) {
-        return this.obj.getElementsByClassName(this.prefix + '-' + className)[0];
+      className: function (className) {
+          return this.obj.getElementsByClassName(this.prefix + '-' + className)[0];
       }
-    };
-    window['ShopCart'] = ShopCart;
-  })(window);
+  };
+  window['ShopCart'] = ShopCart;
+})(window);
