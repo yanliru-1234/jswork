cc03f9716fca7a07161f60a7d62746face237c0e
apple <apple@jd505-49.local>
2019-12-30 14:48:52 +0800

    work20

--- work20/index.js
+++ work20/index.js
@@ -2,7 +2,6 @@
 $('#fold>ul>li:first').find('.wrap').css({
     display: 'block'
 });
-
 $('#fold>ul>li').click(function () {
     $(this).siblings('li').find('.wrap').css({
         display: 'none'
