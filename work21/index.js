c85105c5833cde05ddd50c3e41864e2db551bfcc
yanliru-1234 <2312862225@qq.com>
2019-12-30 23:47:53 +0800

    work18

--- work21/index.js
+++ work21/index.js
@@ -1,73 +1,67 @@
 $(function () {
-    var i = 0;
-    var timer = null;
-    var delay = 1000;
-    var width = 1420;
-    var speed = 400;
-    var firstimg = $('.hot li').first().clone();
-    $('.hot').append(firstimg).width($('.hot li').length * width);
-    timer = setInterval(imgChange, delay);
+    var i = 0
+    var timer = null
+    var delay = 1000 
+    var width = 1420
+    var speed = 400
+    var firstimg = $('.hot li').first().clone()
+    $('.hot').append(firstimg).width($('.hot li').length*width)
+    timer = setInterval(imgChange,delay)
     $('.banner').hover(function () {
-        clearInterval(timer);
-    }, function () {
-        timer = setInterval(imgChange, delay);
-    });
-
-    $('.dot li').monseover(function () {
-        i = $(this).index();
-        $('.hot').stop().animate({
-            left: -i * width
-        }, 200);
-        dotChange();
-    });
-
-    $('.banner').hover(function () {
-        $('.arrow').show();
+        clearInterval(timer)
     },function () {
-        $('.arrow').hide();
-    });
-
+        timer = setInterval(imgChange,delay)
+    })
+    $('.dot li').mouseover(function () {
+        i = $('.hot').stop().animate({
+            left:-i*width
+        },200)
+        dotChange()
+    })
+    $('.banner').hover(function () {
+        $('.arrow').show()
+    },function () {
+        $('.arrow').hide()
+    })
     $('.next').click(function () {
-        imgChange();
-    });
-
+        imgChange()
+    })
     $('.prev').click(function () {
-        --i;
-        if (i == -1) {
-            i = $('.hot li').length - 2;
+        console.log(1)
+        --i
+        if(i==-1){
+            i=$('.hot li').length -2
             $('.hot').css({
-                left: -($('.hot li').length - 1) * width
-            });
+                left:-($('.hot li').length-1) *width
+            })
+            }
+            $('.hot').stop().animate({
+                left:-i*width
+            },speed)
+            dotChange()
+
+    })
+    function imgChange() {
+        ++i
+        isCrack()
+        dotChange()
+    }
+    function isCrack() {
+        if(i==$('.hot li').length){
+            i=1
+            $('.hot').css({
+                left:0
+            })
         }
         $('.hot').stop().animate({
-            left: -i * width
-        }, speed);
-        dotChange();
-    });
-
-    function imgChange(){
-        ++i;
-        isCrack();
-        dotChange();
+            left:-i*width
+        },speed)
     }
-
-    function isCrack(){
-        if (i == $('.hot li').length) {
-            i = 1;
-            $('.hot').css({
-                left: 0
-            });
-        }
-        $('.hot').stop().animate({
-            left: -i * width
-        }, speed);
-    }
-
-    function dotChange(){
-        if (i == $('.hot li').length - 1) {
-            $('.dot li').eq(0).addClass('on').siblings().removeClass('on');
+    function dotChange() {
+        if(i==$('.hot li').length -1){
+            $('.dot li').eq(0).addClass('on').siblings().removeClass('on')
         }else{
-            $('.dot li').eq(i).addClass('on').siblings().removeClass('on');
+            $('.dot li').eq(i).addClass('on').siblings().removeClass('on')
         }
     }
 })
